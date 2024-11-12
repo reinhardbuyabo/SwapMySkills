1c2f6a516d30adcbbd78d536ac5f1c796be8cbcc
reinhard <buyabor@gmail.com>
2024-11-12 10:27:49 +0300

    update: file and folder structure

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 
+// App Instance
 const app = express();
-const port = 5000;
 
+// App Config
+// dotenv.config()
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+// app.use(cors());
+app.use(urlencoded({ extended: true }))
+
+// DB Config
+
+// API Endpoints
 app.get('/', (req, res) => {
     res.send('Hello World!\n');
 });
 
-app.listen(port, () => {
+// Server
+const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+module.exports = server
