c75c07426af6b9f644ba9e582410bc838175ce7d
reinhard <buyabor@gmail.com>
2024-11-13 09:32:11 +0300

    feat: Authentication Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const pool = require("../config/db");
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]; // get the 2nd string from the array
+            console.log(token);
+            // console.log(process.env.JWT_SECRET); this is okay
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decoded); // { id: 12, iat: 1718221335, exp: 1720813335 } // sometimes the id property isn't present
+            employee_result = await pool.query(`SELECT * FROM employee WHERE emp_id=${decoded.id}`);
+
+            if (employee_result.rows !== 0) {
+                req.user = employee_result.rows[0];
+            } else {
+                next(new Error("Token might not be valid"))
+            }
+
+            next();
+        }
+        catch (err) {
+            console.log(err);
+            res.status(401);
+            next(new Error(`Not Authorized: ${err}`));
+        }
+    }
+
+
+    if (!token) {
+        res.status(401);
+        next(new Error("Not Authorized!"))
+    }
+}
+
+module.exports = protect;
